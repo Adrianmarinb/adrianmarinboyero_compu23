b9e22b06e629c10b40eb0da66ec9f628d4391154
Adrianmarinb <e.marinadrian@go.ugr.es>
2023-03-13 18:47:57 +0100

    Probando c++

---
+++ probando.cpp
@@ -0,0 +1,20 @@
+// prueba de un programa en c++
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    cout << " Introduce un valor para a: ";
+    int a, b;
+    int result;
+
+    cin >> a;
+    b = 6;
+
+    result = a - b;
+
+    cout << result << " Adrián es muy guapo";
+
+    return 0;    
+    }
